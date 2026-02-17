bec097ce0c9c0d3c87b8924f86483c7610eb01e7
omerbeithalahmy <omer24883@gmail.com>
2026-02-17 10:40:53 +0200

    complete eks terraform setup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,9 @@ variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
 }
+
+variable "cluster_name" {
+    description = "Name of the EKS cluster"
+    type = string
+    default = "opsguardian-cluster"
+}
