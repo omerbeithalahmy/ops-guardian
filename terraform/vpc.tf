52260ef977570e8445bd30c045edbe8a63d3b3c0
omerbeithalahmy <omer24883@gmail.com>
2026-02-17 10:30:16 +0200

    fix vpc main

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
-data "aws_availability_zone" "available" {}
+data "aws_availability_zones" "available" {}
 
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
@@ -6,7 +6,7 @@ module "vpc" {
 
     name = var.vpc_name
     cidr = var.vpc_cidr
-    azs = slice(data.aws_availability_zone.available, 0, 2)
+    azs = slice(data.aws_availability_zones.available.names, 0, 2)
     private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
     public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
     enable_nat_gateway = true
@@ -21,7 +21,7 @@ module "vpc" {
 
     tags = {
         Terraform = "true"
-        Environmet = "dev"
+        Environment = "dev"
         Project = "opsguardian"
     }
 }
