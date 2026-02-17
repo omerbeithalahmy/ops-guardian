f66e0ca638cfbd6d344ca73628107be1657f578c
omerbeithalahmy <omer24883@gmail.com>
2026-02-17 15:56:23 +0200

    chore: remove ECR and GitHub OIDC from Terraform (now managed manually)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,49 +1,39 @@
-output "repository_url" {
-    description = "The URL of the repository"
-    value = module.ecr.repository_url
-}
-
-output "repository_arn" {
-    description = "The ARN of the repository"
-    value = module.ecr.repository_arn
-}
-
 output "vpc_id" {
-    description = "The ID of the VPC"
-    value = module.vpc.vpc_id
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-    description = "List of IDs of the private subnets"
-    value = module.vpc.private_subnets
+  description = "List of IDs of the private subnets"
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-    description = "List of IDs of the public subnets"
-    value = module.vpc.public_subnets
+  description = "List of IDs of the public subnets"
+  value       = module.vpc.public_subnets
 }
 
 output "cluster_endpoint" {
-    description = "Endpoint for EKS control plane"
-    value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-    description = "Security group ids attached to the cluster control plane"
-    value = module.eks.cluster_security_group_id
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
 }
 
 output "cluster_name" {
-    description = "Name of the EKS cluster"
-    value = module.eks.cluster_name
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
 }
 
 output "github_role_arn" {
-    description = "Role ARN for GitHub Actions"
-    value = aws_iam_role.github_actions.arn
+  description = "Role ARN for GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
 }
 
 output "opsguardian_role_arn" {
-    description = "Role ARN"
-    value = module.opsguardian_irsa_role.iam_role_arn
+  description = "Role ARN"
+  value       = module.opsguardian_irsa_role.iam_role_arn
 }
