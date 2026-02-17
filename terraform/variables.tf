49ec7c189d5b9ac093d637fafb16bea2743436f1
omerbeithalahmy <omer24883@gmail.com>
2026-02-17 10:23:51 +0200

    add vpc terraform code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,15 @@ variable "repo_name" {
     type = string
     default = "opsguardian"
 }
+
+variable "vpc_name" {
+    description = "Name of the VPC"
+    type = string
+    default = "opsguardian-vpc"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
