abed22dfe2ae3638840dfb4d215317b0068a44ac
omerbeithalahmy <omer24883@gmail.com>
2026-02-18 10:02:35 +0200

    level 1: fix irsa wiring, rename serviceaccount file, fix eks desired_size

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,29 +1,29 @@
 module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-    cluster_name = var.cluster_name
-    cluster_version = 1.29
-    cluster_endpoint_public_access = true
-    vpc_id = module.vpc.vpc_id
-    subnet_ids = module.vpc.private_subnets
-    control_plane_subnet_ids = module.vpc.private_subnets
+  cluster_name                   = var.cluster_name
+  cluster_version                = 1.29
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  control_plane_subnet_ids       = module.vpc.private_subnets
 
-    eks_managed_node_groups = {
-        general = {
-            min_size = 1
-            max_size = 2
-            desired_state = 1
+  eks_managed_node_groups = {
+    general = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-            instance_types = ["t3.medium"]
-            capacity_type = "ON_DEMAND"
-        }
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
+  }
 
-    enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = true
 
-    tags = {
-        Environment = "dev"
-        Project = "opsguardian"
-    }
+  tags = {
+    Environment = "dev"
+    Project     = "opsguardian"
+  }
 }
